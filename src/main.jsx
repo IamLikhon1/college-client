e7ea5cd459e6eff5705c879640497ed1831bb628
IamLikhon1 <likhonadnan55@gmail.com>
2023-07-22 11:01:41 +0600

    added authprovider

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,10 @@ import {
 } from "react-router-dom";
 import Main from './Layout/Main';
 import Home from './Pages/Home/Home/Home';
+import Colleges from './Pages/Colleges/Colleges';
+import Admission from './Pages/Admissions/Admission';
+import MyColleges from './Pages/MyColleges/MyColleges';
+import AuthProvider from './provider/AuthProvider';
 
 const router = createBrowserRouter([
   {
@@ -16,6 +20,18 @@ const router = createBrowserRouter([
       {
         path:'/',
         element:<Home></Home>
+      },
+      {
+        path:'/colleges',
+        element:<Colleges></Colleges>
+      },
+      {
+        path:'/admission',
+        element:<Admission></Admission>
+      },
+      {
+        path:'/myCollege',
+        element:<MyColleges></MyColleges>
       }
     ]
 
@@ -24,6 +40,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <RouterProvider router={router} />
+     <AuthProvider><RouterProvider router={router} /></AuthProvider>
   </React.StrictMode>,
 )
